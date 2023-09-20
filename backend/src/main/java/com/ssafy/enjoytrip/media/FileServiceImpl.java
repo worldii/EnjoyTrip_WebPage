c8e1ccb7fe51eabb6b14584d55448fd97fd90c2b
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-09-21 07:19:20 +0900

    refactor: file Service refactor

--- backend/src/main/java/com/ssafy/enjoytrip/media/FileServiceImpl.java
+++ backend/src/main/java/com/ssafy/enjoytrip/media/FileServiceImpl.java
@@ -1,29 +1,29 @@
-package com.ssafy.enjoytrip.board.service;
+package com.ssafy.enjoytrip.media;
 
 import com.ssafy.enjoytrip.board.model.dto.FileInfo;
 import com.ssafy.enjoytrip.board.model.mapper.BoardMapper;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
+
     private final BoardMapper boardMapper;
     private final S3Service s3Service;
 
     @Override
-    public int insertFile(int boardId, List<MultipartFile> imageFiles,String folder) {
-        // imageFile을 s3Service dml uploadtoS3 함수를 호출하여 url 을 가져온 다음 set 함 그리고 그것을
-        // List<fileinfo> 로 변환
+    public int insertFile(final Long boardId, final List<MultipartFile> imageFiles,
+        final String folder) {
+        
         List<FileInfo> list = new ArrayList<>();
         imageFiles.forEach(imageFile -> {
             try {
                 String fileUrl = s3Service.uploadMediaToS3(imageFile, folder);
-                list.add(new FileInfo(0, boardId, fileUrl));
+                list.add(new FileInfo(0L, boardId, fileUrl));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -32,13 +32,7 @@ public class FileServiceImpl implements FileService {
     }
 
     @Override
-    public int deleteFile(int boardId) {
-        return 0;
-    }
-
-    @Override
-    public List<FileInfo> selectFile(int boardId) {
+    public List<FileInfo> selectFile(Long boardId) {
         return boardMapper.selectFile(boardId);
     }
-
 }
