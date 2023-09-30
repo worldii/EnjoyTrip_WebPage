a4b84e5fd16369a4a13bcf0e523062ac70f761a3
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-10-01 04:22:23 +0900

    test: fileInfo 도메인 테스트

--- backend/src/main/java/com/ssafy/enjoytrip/media/model/entity/FileInfo.java
+++ backend/src/main/java/com/ssafy/enjoytrip/media/model/entity/FileInfo.java
@@ -17,7 +17,21 @@ public class FileInfo {
 
     @Builder
     public FileInfo(final Long boardId, final String fileUrl) {
+        validateBoardId(boardId);
+        validateFileUrl(fileUrl);
         this.boardId = boardId;
         this.fileUrl = fileUrl;
+    }
+
+    private void validateFileUrl(final String fileUrl) {
+        if (fileUrl == null || fileUrl.isEmpty()) {
+            throw new IllegalArgumentException("file의 url은 null이거나 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateBoardId(final Long boardId) {
+        if (boardId == null) {
+            throw new IllegalArgumentException("boardId는 null이 될 수 없습니다.");
+        }
     }
 }
---
+++ backend/src/test/java/com/ssafy/enjoytrip/domain/media/FileInfoTest.java
@@ -0,0 +1,46 @@
+package com.ssafy.enjoytrip.domain.media;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import com.ssafy.enjoytrip.media.model.entity.FileInfo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
+@DisplayName("FileInfo 테스트")
+class FileInfoTest {
+
+    @Test
+    @DisplayName("fileInfo 정상적으로 생성")
+    void fileInfoCreate() {
+        // given
+        assertThatCode(() -> FileInfo.builder()
+            .fileUrl("https://www.naver.com")
+            .boardId(1L)
+            .build()).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("fileInfo 의 fileUrl이 null일 때 예외 발생")
+    void fileInfoCreateFailWithFileUrlNull(String fileUrl) {
+        // given
+        assertThatCode(() -> FileInfo.builder()
+            .fileUrl(fileUrl)
+            .boardId(1L)
+            .build()).hasMessage("file의 url은 null이거나 비어있을 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @DisplayName("fileInfo 의 boardId가 null일 때 예외 발생")
+    void fileInfoCreateFailWithBoardIdNull(Long boardId) {
+        // given
+        assertThatCode(() -> FileInfo.builder()
+            .fileUrl("https://www.naver.com")
+            .boardId(boardId)
+            .build()).hasMessage("boardId는 null이 될 수 없습니다.");
+    }
+}
