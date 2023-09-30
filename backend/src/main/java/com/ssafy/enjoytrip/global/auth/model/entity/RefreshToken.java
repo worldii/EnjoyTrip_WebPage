23d8e69f42d15c401fc4cd2bc60f203f844747f6
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-09-30 22:27:39 +0900

    test: RefreshToken 도메인 테스트

--- backend/src/main/java/com/ssafy/enjoytrip/global/auth/model/entity/RefreshToken.java
+++ backend/src/main/java/com/ssafy/enjoytrip/global/auth/model/entity/RefreshToken.java
@@ -2,14 +2,32 @@ package com.ssafy.enjoytrip.global.auth.model.entity;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 
 @Getter
 @Builder
-@RequiredArgsConstructor
 public class RefreshToken {
 
     private final String tokenName;
     private final String userId;
+
+    public RefreshToken(final String tokenName, final String userId) {
+        validateTokenName(tokenName);
+        validateUserId(userId);
+
+        this.tokenName = tokenName;
+        this.userId = userId;
+    }
+
+    private void validateTokenName(final String tokenName) {
+        if (tokenName == null || tokenName.isEmpty()) {
+            throw new IllegalArgumentException("토큰이 존재하지 않습니다.");
+        }
+    }
+
+    private void validateUserId(final String userId) {
+        if (userId == null || userId.isEmpty()) {
+            throw new IllegalArgumentException("사용자 아이디가 존재하지 않습니다.");
+        }
+    }
 }
