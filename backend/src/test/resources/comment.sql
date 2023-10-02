853d75e69b0c3660573c55103f72d690bedd65bc
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-10-02 11:23:12 +0900

    fix: comment 테스트 고침

--- backend/src/test/resources/comment.sql
+++ backend/src/test/resources/comment.sql
@@ -1,2 +1,2 @@
-insert into comment (comment_id, content, current_update, board_id, user_id)
+insert into comment (content, current_update, board_id, user_id)
 values ('comment1', '2019-01-01 00:00:00', 1, 1);
