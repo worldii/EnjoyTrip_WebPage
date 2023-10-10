f1e4d01359010c8511cc1570fcdc6a12f5370583
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-10-10 12:30:04 +0900

    test: HotPlace 도메인 테스트

--- backend/src/main/java/com/ssafy/enjoytrip/core/hotplace/model/entity/HotPlace.java
+++ backend/src/main/java/com/ssafy/enjoytrip/core/hotplace/model/entity/HotPlace.java
@@ -1,5 +1,6 @@
 package com.ssafy.enjoytrip.core.hotplace.model.entity;
 
+import com.ssafy.enjoytrip.global.error.HotPlaceException;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,16 +10,94 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Builder
 public class HotPlace {
 
     private String hotPlaceId;
     private String hotPlaceName;
-    private String x;
-    private String y;
+    private Double x;
+    private Double y;
     private Long vote;
     private String placeUrl;
     private String roadAddressName;
     private String addressName;
     private List<HotPlaceArticle> hotPlaceArticles;
+    private List<HotPlaceTag> hotPlaceTags;
+
+    @Builder
+    public HotPlace(
+        final String hotPlaceId,
+        final String hotPlaceName,
+        final Double x, final Double y,
+        final Long vote, final String placeUrl,
+        final String roadAddressName, final String addressName
+    ) {
+        validateHotPlaceId(hotPlaceId);
+        validateHotPlaceName(hotPlaceName);
+        validateX(x);
+        validateY(y);
+        validatePlaceUrl(placeUrl);
+        validateRoadAddressName(roadAddressName);
+        validateAddressName(addressName);
+
+        this.hotPlaceId = hotPlaceId;
+        this.hotPlaceName = hotPlaceName;
+        this.x = x;
+        this.y = y;
+        this.vote = vote;
+        this.placeUrl = placeUrl;
+        this.roadAddressName = roadAddressName;
+        this.addressName = addressName;
+    }
+
+    private void validateHotPlaceId(final String hotPlaceId) {
+        if (hotPlaceId == null || hotPlaceId.isEmpty()) {
+            throw new HotPlaceException("핫플레이스 아이디가 없습니다.");
+        }
+    }
+
+    private void validateHotPlaceName(final String hotPlaceName) {
+        if (hotPlaceName == null || hotPlaceName.isEmpty()) {
+            throw new HotPlaceException("핫플레이스 이름이 없습니다.");
+        }
+    }
+
+
+    private void validateX(final Double x) {
+        if (x == null) {
+            throw new HotPlaceException("x좌표가 없습니다.");
+        }
+    }
+
+    private void validateY(Double y) {
+        if (y == null) {
+            throw new HotPlaceException("y좌표가 없습니다.");
+        }
+    }
+
+
+    private void validateAddressName(final String addressName) {
+        if (addressName == null || addressName.isEmpty()) {
+            throw new HotPlaceException("주소가 없습니다.");
+        }
+    }
+
+    private void validateRoadAddressName(final String roadAddressName) {
+        if (roadAddressName == null || roadAddressName.isEmpty()) {
+            throw new HotPlaceException("도로명 주소가 없습니다.");
+        }
+    }
+
+    private void validatePlaceUrl(final String placeUrl) {
+        if (placeUrl == null || placeUrl.isEmpty()) {
+            throw new HotPlaceException("핫플레이스 URL이 없습니다.");
+        }
+    }
+    
+    public void setHotPlaceTagList(final List<HotPlaceTag> hotPlaceTagList) {
+        this.hotPlaceTags = hotPlaceTagList;
+    }
+
+    public void updateVoteCount(final Long voteCount) {
+        this.vote = voteCount;
+    }
 }
---
+++ backend/src/test/java/com/ssafy/enjoytrip/domain/hotplace/HotPlaceTest.java
@@ -0,0 +1,221 @@
+package com.ssafy.enjoytrip.domain.hotplace;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import com.ssafy.enjoytrip.core.hotplace.model.entity.HotPlace;
+import com.ssafy.enjoytrip.global.error.HotPlaceException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+@DisplayName("HotPlace 테스트")
+class HotPlaceTest {
+
+    @Test
+    @DisplayName("HotPlace 를 정상적으로 생성 한다")
+    void createHotPlaceSuccess() {
+        // given
+        String hotPlaceId = "1";
+        String hotPlaceName = "서울";
+        String addressName = "서울";
+        String placeUrl = "www.naver.com";
+        Double x = 1.0;
+        Double y = 1.0;
+        String roadAddressName = "서울";
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("HotPlaceId가 null일 경우, 예외가 발생한다")
+    void createHotPlaceFailByHotPlaceId(String hotPlaceId) {
+        // given
+        String hotPlaceName = "서울";
+        String addressName = "서울";
+        String placeUrl = "www.naver.com";
+        Double x = 1.0;
+        Double y = 1.0;
+        String roadAddressName = "서울";
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .isInstanceOf(HotPlaceException.class)
+            .hasMessage("핫플레이스 아이디가 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("HotPlaceName이 null일 경우, 예외가 발생한다")
+    void createHotPlaceFailByHotPlaceName(String hotPlaceName) {
+        // given
+        String hotPlaceId = "1";
+        String addressName = "서울";
+        String placeUrl = "www.naver.com";
+        Double x = 1.0;
+        Double y = 1.0;
+        String roadAddressName = "서울";
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .isInstanceOf(HotPlaceException.class)
+            .hasMessage("핫플레이스 이름이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("HotPlace 좌표의 X가 null일 경우, 예외가 발생한다")
+    void createHotPlaceFailByX() {
+        // given
+        String hotPlaceId = "1";
+        String hotPlaceName = "서울";
+        String addressName = "서울";
+        String placeUrl = "www.naver.com";
+        Double x = null;
+        Double y = 1.0;
+        String roadAddressName = "서울";
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .isInstanceOf(HotPlaceException.class)
+            .hasMessage("x좌표가 없습니다.");
+    }
+
+    @Test
+    @DisplayName("HotPlace 좌표의 Y가 null일 경우, 예외가 발생한다")
+    void createHotPlaceFailByY() {
+        // given
+        String hotPlaceId = "1";
+        String hotPlaceName = "서울";
+        String addressName = "서울";
+        String placeUrl = "www.naver.com";
+        Double x = 1.0;
+        Double y = null;
+        String roadAddressName = "서울";
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .isInstanceOf(HotPlaceException.class)
+            .hasMessage("y좌표가 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("HotPlace 주소가 null일 경우, 예외가 발생한다")
+    void createHotPlaceFailByAddressName(String addressName) {
+        // given
+        String hotPlaceId = "1";
+        String hotPlaceName = "서울";
+        String placeUrl = "www.naver.com";
+        Double x = 1.0;
+        Double y = 1.0;
+        String roadAddressName = "서울";
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .isInstanceOf(HotPlaceException.class)
+            .hasMessage("주소가 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("HotPlace 도로명 주소가 null일 경우, 예외가 발생한다")
+    void createHotPlaceFailByRoadAddressName(String roadAddressName) {
+        // given
+        String hotPlaceId = "1";
+        String hotPlaceName = "서울";
+        String addressName = "서울";
+        String placeUrl = "www.naver.com";
+        Double x = 1.0;
+        Double y = 1.0;
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .isInstanceOf(HotPlaceException.class)
+            .hasMessage("도로명 주소가 없습니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("HotPlace URL이 null일 경우, 예외가 발생한다")
+    void createHotPlaceFailByPlaceUrl(String placeUrl) {
+        // given
+        String hotPlaceId = "1";
+        String hotPlaceName = "서울";
+        String addressName = "서울";
+        Double x = 1.0;
+        Double y = 1.0;
+        String roadAddressName = "서울";
+
+        // when & then
+        assertThatCode(() -> HotPlace.builder()
+            .hotPlaceId(hotPlaceId)
+            .hotPlaceName(hotPlaceName)
+            .addressName(addressName)
+            .placeUrl(placeUrl)
+            .x(x)
+            .y(y)
+            .roadAddressName(roadAddressName)
+            .build())
+            .isInstanceOf(HotPlaceException.class)
+            .hasMessage("핫플레이스 URL이 없습니다.");
+    }
+}
