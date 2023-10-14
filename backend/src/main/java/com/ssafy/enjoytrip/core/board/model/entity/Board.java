d9501f53b4c807fe9c2181bc6ae1611133bf72b5
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-10-15 06:31:57 +0900

    test: board 도메인 테스트

--- backend/src/main/java/com/ssafy/enjoytrip/core/board/model/entity/Board.java
+++ backend/src/main/java/com/ssafy/enjoytrip/core/board/model/entity/Board.java
@@ -1,13 +1,12 @@
 package com.ssafy.enjoytrip.core.board.model.entity;
 
+import com.ssafy.enjoytrip.global.error.BoardException;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Board {
@@ -30,5 +29,41 @@ public class Board {
         return imageUrls == null || imageUrls.isEmpty();
     }
 
+    @Builder
+    public Board(
+        final Long boardId, final String userId, final String subject,
+        final String content, final Long hit, final String currentUpdate,
+        final BoardType boardType, final List<String> imageUrls
+    ) {
+        validateUserId(userId);
+        validateSubject(subject);
+        validateContent(content);
 
+        this.boardId = boardId;
+        this.userId = userId;
+        this.subject = subject;
+        this.content = content;
+        this.hit = hit;
+        this.currentUpdate = currentUpdate;
+        this.boardType = boardType;
+        this.imageUrls = imageUrls;
+    }
+
+    private void validateUserId(String userId) {
+        if (userId == null || userId.isEmpty()) {
+            throw new BoardException("유저 아이디가 없습니다.");
+        }
+    }
+
+    private void validateSubject(String subject) {
+        if (subject == null || subject.isEmpty()) {
+            throw new BoardException("제목이 없습니다.");
+        }
+    }
+
+    private void validateContent(String content) {
+        if (content == null || content.isEmpty()) {
+            throw new BoardException("내용이 없습니다.");
+        }
+    }
 }
---
+++ backend/src/test/java/com/ssafy/enjoytrip/domain/board/BoardTest.java
@@ -0,0 +1,71 @@
+package com.ssafy.enjoytrip.domain.board;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import com.ssafy.enjoytrip.core.board.model.entity.Board;
+import com.ssafy.enjoytrip.core.board.model.entity.BoardType;
+import com.ssafy.enjoytrip.global.error.BoardException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Board 도메인 테스트")
+class BoardTest {
+
+    @Test
+    @DisplayName("게시판 생성 테스트")
+    void createBoard() {
+        // given
+        Board board = Board.builder()
+            .boardType(BoardType.COMMUNITY)
+            .subject("test")
+            .userId("test")
+            .content("test")
+            .imageUrls(null)
+            .build();
+    }
+
+    @Test
+    @DisplayName("게시판 생성 시 제목이 없으면 예외 발생")
+    void createBoardWithoutSubject() {
+        // given & when & then
+        assertThatCode(() -> Board.builder()
+            .boardType(BoardType.COMMUNITY)
+            .subject(null)
+            .userId("test")
+            .content("test")
+            .imageUrls(null)
+            .build())
+            .isInstanceOf(BoardException.class)
+            .hasMessage("제목이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("게시판 생성 시 내용이 없으면 예외 발생")
+    void createBoardWithoutContent() {
+        // given & when & then
+        assertThatCode(() -> Board.builder()
+            .boardType(BoardType.COMMUNITY)
+            .subject("test")
+            .userId("test")
+            .content(null)
+            .imageUrls(null)
+            .build())
+            .isInstanceOf(BoardException.class)
+            .hasMessage("내용이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("게시판 생성 시 작성자가 없으면 예외 발생")
+    void createBoardWithoutUserId() {
+        // given & when & then
+        assertThatCode(() -> Board.builder()
+            .boardType(BoardType.COMMUNITY)
+            .subject("test")
+            .userId(null)
+            .content("test")
+            .imageUrls(null)
+            .build())
+            .isInstanceOf(BoardException.class)
+            .hasMessage("유저 아이디가 없습니다.");
+    }
+}
